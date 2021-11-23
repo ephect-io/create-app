ad86a2fede31f4075ab36c9a6e89472142629331
David Blanchard <dpjb@hotmail.fr>
2021-11-23 19:20:29 +0100

    Update root bootstrap

--- bootstrap.php
+++ bootstrap.php
@@ -1,5 +1,5 @@
 <?php
 
-define('EGG_FRAMEWORK', file_get_contents('config/framework'));
+define('EGG_FRAMEWORK', file_get_contents(dirname(__FILE__) . DIRECTORY_SEPARATOR . 'config' . DIRECTORY_SEPARATOR . 'framework'));
 
 include EGG_FRAMEWORK . DIRECTORY_SEPARATOR . 'bootstrap.php';
