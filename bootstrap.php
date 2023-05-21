2fad99c70bccc347b3fe0171f270431170c3d5d9
David Blanchard <dpjb@hotmail.fr>
2023-05-21 12:02:20 +0200

    Add Composer autoload

--- bootstrap.php
+++ bootstrap.php
@@ -3,3 +3,4 @@
 define('EGG_FRAMEWORK', file_get_contents(dirname(__FILE__) . DIRECTORY_SEPARATOR . 'config' . DIRECTORY_SEPARATOR . 'framework'));
 
 include EGG_FRAMEWORK . DIRECTORY_SEPARATOR . 'bootstrap.php';
+include 'vendor/autoload.php';
