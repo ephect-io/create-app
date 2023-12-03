aa8caa5c86ad2be97e28c43f1f7c6739577b031e
David Blanchard <david.blanchard71@icloud.com>
2023-12-03 13:25:15 +0100

    Implement HelloWord webcomponent as a replacement sample for TextWriter

---
+++ app/WebComponents/HelloWord/HelloWord.class.mjs
@@ -0,0 +1,13 @@
+class HelloWord
+{
+
+    sayHello(word) {
+        /**
+         * Do something with the parameters
+         */
+        return 'Hello ' + word;
+    }
+
+}
+
+export default HelloWord
